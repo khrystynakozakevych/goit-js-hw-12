51b557fd3b1dbee5e676c2f091bc4b4cb14a8bfe
khrystyna_kozakevych <margaret.gray.777@gmail.com>
2024-12-12 22:48:24 +0100

    update

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,23 +1,21 @@
+import axios from 'axios';
+
 // pixabay api
 const API_KEY = '47584344-4fbbfc2b4e03cd845aed0766f';
 const BASE_URL = 'https://pixabay.com/api/';
 
 // fetching the images from the server
-export function fetchImages(query) {
+export async function fetchImages(query, page) {
   const params = new URLSearchParams({
     key: API_KEY,
     q: query,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: 'true',
+    page,
+    per_page: 15,
   });
 
-  const url = `${BASE_URL}?${params.toString()}`;
-
-  return fetch(url).then(response => {
-    if (!response.ok) {
-      throw new Error('Failed to fetch images');
-    }
-    return response.json();
-  });
+  const response = await axios.get(BASE_URL, { params });
+  return response.data;
 }
--- src/main.js
+++ src/main.js
@@ -16,11 +16,14 @@ const input = document.querySelector('.search-input');
 const loader = document.querySelector('.loader');
 const loadMoreBtn = document.querySelector('.load-more-btn');
 
+let query = '';
+let page = 1;
+
 // adding event listener to the input
-form.addEventListener('submit', event => {
+form.addEventListener('submit', async event => {
   event.preventDefault();
 
-  const query = input.value.trim();
+  query = input.value.trim();
   if (!query) {
     iziToast.error({
       message: 'Please enter a search term.',
@@ -34,36 +37,80 @@ form.addEventListener('submit', event => {
   // clearing the gallery and showing the loader
   clearGallery();
   showLoader(loader);
+  loadMoreBtn.classList.add('hidden');
+  page = 1; // resetting the page number
 
   // fetching the images from the server
-  fetchImages(query)
-    .then(data => {
-      hideLoader(loader); // hiding the loader
+  try {
+    const data = await fetchImages(query, page);
+    hideLoader(loader); // hiding the loader
 
-      // sending the warning if the are no images for the query
-      if (data.hits.length === 0) {
-        iziToast.warning({
-          message: 'Sorry, there are no images matching your search query.',
-          position: 'topRight',
-          backgroundColor: '#ef4040',
-          class: 'custom-toast-error',
-        });
-        return;
-      }
-
-      // rendering the gallery with query informations
-      renderGallery(data.hits);
-    })
-
-    // sending the warning if there's an error
-    .catch(error => {
-      hideLoader(loader);
-      iziToast.error({
-        message: 'Something went wrong. Please try again.',
+    // sending the warning if there are no images for the query
+    if (data.hits.length === 0) {
+      iziToast.warning({
+        message: 'Sorry, there are no images matching your search query.',
         position: 'topRight',
         backgroundColor: '#ef4040',
         class: 'custom-toast-error',
       });
-      console.error(error);
+      return;
+    }
+
+    // rendering the gallery with query informations
+    renderGallery(data.hits);
+
+    // adding load more btn if there are moro imsges to show
+    if (data.totalHits > page * 15) {
+      loadMoreBtn.classList.remove('hidden');
+    }
+  } catch (error) {
+    // sending the warning if there's an error
+    hideLoader(loader);
+    iziToast.error({
+      message: 'Something went wrong. Please try again.',
+      position: 'topRight',
+      backgroundColor: '#ef4040',
+      class: 'custom-toast-error',
     });
+    console.error(error);
+  }
+});
+
+// adding event listener to the Load More btn
+loadMoreBtn.addEventListener('click', async () => {
+  page += 1;
+  showLoader(loader);
+
+  // fetching more images from the server
+  try {
+    const data = await fetchImages(query, page);
+    hideLoader(loader);
+
+    if (data.hits.length === 0 || data.totalHits <= page * 15) {
+      iziToast.warning({
+        message: "We're sorry, but you've reached the end of search results.",
+        position: 'topRight',
+      });
+      loadMoreBtn.classList.add('hidden');
+      return;
+    }
+
+    renderGallery(data.hits);
+
+    // smooth scrolling
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+  } catch (error) {
+    hideLoader(loader);
+    iziToast.error({
+      message: 'Something went wrong. Please try again.',
+      position: 'topRight',
+    });
+    console.error(error);
+  }
 });
